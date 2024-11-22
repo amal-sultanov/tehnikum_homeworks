f23d36004eded0704b1eb5551530a9fe6dd56e98
AmalSultanov <pogboom1014@gmail.com>
2024-11-22 20:46:14 +0500

    removed token

--- section2/lesson17_hw/main.py
+++ section2/lesson17_hw/main.py
@@ -2,7 +2,7 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('7882237645:AAEogmFBpCAQfl5uocSz9B41A9eo4Znwrx4')
+bot = telebot.TeleBot('')
 
 
 @bot.message_handler(commands=['start'])
